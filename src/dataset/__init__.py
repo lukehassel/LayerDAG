14c9c2f4c4f0278b7283098a4126d9a4742d2faa
Luke Hassel <lukehassel@users.noreply.github.com>
2025-10-03 20:21:46 +0200

    Fix dataset loader: raise NotImplementedError instead of return

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,9 +1,12 @@
 from .layer_dag import *
 from .general import DAGDataset
 from .tpu_tile import get_tpu_tile
+from .quantum_circuit import get_quantum_circuit
 
 def load_dataset(dataset_name):
     if dataset_name == 'tpu_tile':
         return get_tpu_tile()
+    elif dataset_name == 'quantum_circuit':
+        return get_quantum_circuit()
     else:
-        return NotImplementedError
+        raise NotImplementedError(f"Dataset {dataset_name} not supported")
